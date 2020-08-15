aaf591826cf7e06e9b2418f1123daf6655b78746
Harsha-Reddy <mhvr226@gmail.com>
2020-08-15 16:11:44 +0530

    added ssl

--- Amazon.py
+++ Amazon.py
@@ -1,6 +1,11 @@
 from urllib.request import urlopen
 import json
 import urllib
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 urlpresent = True
 url = 'https://www.amazon.com/s?k=mens+sunglasses&ref=nb_sb_noss_2'
